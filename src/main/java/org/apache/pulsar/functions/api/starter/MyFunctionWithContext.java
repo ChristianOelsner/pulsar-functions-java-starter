2977cb441f0000af938b811eac9ffb6f82269afb
Luc Perkins <lucperkins@gmail.com>
2018-03-08 12:30:56 -0800

    add function with context

---
+++ src/main/java/org/apache/pulsar/functions/api/starter/MyFunctionWithContext.java
@@ -0,0 +1,11 @@
+package org.apache.pulsar.functions.api.starter;
+
+import org.apache.pulsar.functions.api.Context;
+import org.apache.pulsar.functions.api.PulsarFunction;
+
+public class MyFunctionWithContext implements PulsarFunction<String, String> {
+    @Override
+    public String process(String input, Context context) {
+        return
+    }
+}
