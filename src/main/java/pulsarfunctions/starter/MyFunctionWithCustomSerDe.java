0531792c67729cceae7cb60150820ca056e17471
Luc Perkins <lucperkins@gmail.com>
2018-03-08 14:24:00 -0800

    Add processing logic to custom serde function

--- src/main/java/pulsarfunctions/starter/MyFunctionWithCustomSerDe.java
+++ src/main/java/pulsarfunctions/starter/MyFunctionWithCustomSerDe.java
@@ -1,4 +1,12 @@
 package pulsarfunctions.starter;
 
-public class MyFunctionWithCustomSerDe {
+import pulsarfunctions.starter.serde.Tweet;
+
+import java.util.function.Function;
+
+public class MyFunctionWithCustomSerDe implements Function<Tweet, Void> {
+    @Override
+    public void apply(Tweet input) {
+        return null;
+    }
 }
