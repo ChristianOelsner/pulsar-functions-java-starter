8d153bbe20f114a6a20450e2da53b2eb48bf67a9
Luc Perkins <lucperkins@gmail.com>
2018-04-10 14:24:02 -0700

    Add Kryo example for SerDe

--- src/main/java/pulsarfunctions/starter/serde/TweetSerDe.java
+++ src/main/java/pulsarfunctions/starter/serde/TweetSerDe.java
@@ -1,18 +1,46 @@
 package pulsarfunctions.starter.serde;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import org.apache.pulsar.functions.api.SerDe;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class TweetSerDe implements SerDe<Tweet> {
-    @Override
-    public Tweet deserialize(byte[] input) {
-        String username = new String(input);
-        Tweet t = new Tweet();
-        t.setUsername(username);
-        return t;
+    private final Kryo kryo;
+
+    public TweetSerDe() {
+        kryo = new Kryo();
+        kryo.register(Tweet.class, 1);
     }
 
     @Override
-    public byte[] serialize(Tweet input) {
-        return input.getUsername().getBytes();
+    public Tweet deserialize(byte[] bytes) {
+        Tweet tweet;
+        try (ByteArrayInputStream in = new ByteArrayInputStream(bytes)) {
+            Input input = new Input(in);
+            tweet = kryo.readObject(input, Tweet.class);
+            input.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return tweet;
+    }
+
+    @Override
+    public byte[] serialize(Tweet tweet) {
+        byte[] bytes;
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            Output output = new Output(out);
+            kryo.writeObject(output, tweet);
+            bytes = output.toBytes();
+            output.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return bytes;
     }
 }
